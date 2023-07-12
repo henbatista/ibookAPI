cbaf8268c9d2028cf79a0085627a9aef4b208d2c
HENRIQUE BATISTA <85231417+henbatista@users.noreply.github.com>
2023-07-12 10:26:32 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+
+const app = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults({ noCors: false })
+
+const port = process.env.PORT || 3333
+
+app.db = router.db
+
+app.use(middlewares);
+app.use(auth)
+app.use(router)
+app.listen(port)
